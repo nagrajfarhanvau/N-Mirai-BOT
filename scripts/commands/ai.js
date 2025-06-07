af4ca42b12a93d06b5b8a12c33fb7ea2455cb7c6
NASTIk-er <balermeta@gmail.com>
2025-06-07 19:52:31 +0600

    Add files via upload

--- scripts/commands/ai.js
+++ scripts/commands/ai.js
@@ -1,39 +1,57 @@
 module.exports = {
   config: {
-    name: "ai",
-    version: "1.0.0",
-    permission: 0,
-    credits: "Nayan",
-    description: "",
-    prefix: true,
-    category: "user",
-    usages: "query",
-    cooldowns: 5,
-    dependencies: {
-    }
+  name: "ai",
+  version: "1.0.0",
+  permission: 0,
+  credits: "ArYan",
+  description: "",
+  prefix: 'awto', 
+  category: "user", 
+  usages: "query",
+  cooldowns: 5,
+  dependencies: {
+    "nayan-server": ''
+  }
   },
 
   start: async function({ nayan, events, args, Users, NAYAN }) {
-    const axios = require("axios");
-    const request = require("request");
-    const fs = require("fs-extra");
-    const id = nayan.getCurrentUserID()
-    const uid = events.senderID;
-    const nn = await Users.getNameUser(uid);
-    const np = args.join(" ");
 
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const uid = events.senderID;
+  var nn = await Users.getNameUser(uid);
+  let np = args.join(" ");
+  const { gpt } = require("nayan-server");
 
-    try {
-      const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN-07/Nayan/main/api.json');
-      const apiss = apis.data.api;
-      const response = await axios.get(`${apiss}/nayan/gpt3?prompt=${encodeURIComponent(np)}`);
-      const aiResponse = response.data.response || 'I am unable to process your request at the moment.';
-
-
-        await NAYAN.sendContact(aiResponse, id, events.threadID);
-
-    } catch (error) {
-      console.error("Error while processing GPT request:", error);
+gpt({
+    messages: [
+        {
+            role: "assistant",
+            content: "-𝗛𝗲𝗹𝗹𝗼! 𝗛𝗼𝘄 𝗮𝗿𝗲 𝘆𝗼𝘂 𝘁𝗼𝗱𝗮𝘆-?😵"
+        },
+        {
+            role: "user",
+            content: `-𝗛𝗲𝗹𝗹𝗼, 𝗺𝘆 𝗻𝗮𝗺𝗲 𝗶𝘀_//-♦ ${nn}.`
+        },
+        {
+            role: "assitant",
+            content: `-𝗛𝗲𝗹𝗹𝗼, ${nn}! 𝗛𝗼𝘄 𝗮𝗿𝗲 𝘆𝗼𝘂 𝘁𝗼𝗱𝗮𝘆-?`
+        }
+    ],
+    prompt: `${np}`,
+    model: "GPT-4",
+    markdown: false
+}, (err, data) => {
+    console.log(data)
+  const answer = data.gpt
+    var msg = [];
+    {
+        msg += `  ●❯────────────────❮●\n         -♦𝐓𝐀𝐍𝐕𝐈𝐑-𝐁𝐎𝐓♦-         \n●❯────────────────❮●\n❇━━━━━━♦━━━━━❇\n ${answer}\n❇━━━━━━♦━━━━━❇`
     }
+     NAYAN.react("🔰")
+    return NAYAN.reply(msg)
+  });
+
   }
 };
